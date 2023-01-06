5ac30dd2231311b14674525a7fb8537975a785b0
Samantha Wu <samanthayywu@gmail.com>
2023-01-06 17:52:10 +0000

    input func

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+)
+
+func Input(s string, r *bufio.Reader) (string, error) {
+	fmt.Print(s)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
