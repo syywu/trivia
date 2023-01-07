6ce31c280ad22909c9facf93539aed6d5df4a6c7
Samantha Wu <samanthayywu@gmail.com>
2023-01-07 14:29:32 +0000

    Q1

--- main.go
+++ main.go
@@ -16,9 +16,29 @@ func Input(s string, r *bufio.Reader) (string, error) {
 
 func AskName() {
 	name, _ := Input("What is your name?", bufio.NewReader(os.Stdin))
-	fmt.Println("Hi", name)
+	fmt.Println("Welcome to Trivia", name)
+}
+
+// Trivia Question: In a website browser address bar, what does “www” stand for?
+// Answer: World Wide Web
+func Q1() {
+	q, _ := Input("In a website browser address bar, what does “www” stand for?\na- Word Wide West\nb- World Wild Web\nc- World Wide Web\nd-World Wide Window\n", bufio.NewReader(os.Stdin))
+	switch q {
+	case "a":
+		fmt.Println("Wrong answer")
+	case "b":
+		fmt.Println("Wrong answer")
+	case "c":
+		fmt.Println("Correct")
+	case "d":
+		fmt.Println("Wrong answer")
+	default:
+		fmt.Println("Invalid option")
+		Q1()
+	}
 }
 
 func main() {
 	AskName()
+	Q1()
 }
